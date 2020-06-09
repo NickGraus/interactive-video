be6e870251c60246708ec928a2a69e52cf9a0f6a
NickGraus <nick-graus@hotmail.com>
2020-06-09 09:51:42 +0200

    pull video names from json file

--- scripts/main.js
+++ scripts/main.js
@@ -1,60 +1,42 @@
-let videos = [
-    {
-        name: "Work at Brainport.mp4",
-        option1: [
-            text: "Click here to restart the video",
-            reference: "Work at Brainport.mp4"
-        ],
-        option2: [
-            text: [
-                "Click here to see how Brainport helps with the Corona crisis"
-            ]
-            reference: [
-                "Corona-brainport.mp4"
-            ]
-        ],
-        option3: [
-            text: "Click here to see how Brainport supports it region",
-            reference: "Donatie-brainport.mp4"
-        ],
-        qrCode: [
-            qrImage: "Click here to see how Brainport supports it region",
-            qrText: "Scan te QR-code to get more info"
-        ],
-    }
-]
-
-console.log(videos.name);
 let vid = document.getElementById("v0");
 
-let randomVid = videos[Math.floor(Math.random()* videos.length)];
-vid.src= randomVid + ".mp4";
-vid.onended = function() {
-    var sections = document.getElementsByClassName("section");
-    for(var i = 0; i < sections.length; i++){
-		sections[i].style.visibility = "visible";
-	}
+const fetchJSONFile = (path, callback) => {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+         }
+    };
+    httpRequest.open("GET", path);
+    httpRequest.send();
 };
 
+fetchJSONFile("../JSON/videos.json", function(data) {
+    let randomVid = data.videos[Math.floor(Math.random()* data.videos.length)];
+    let videoId = randomVid.id;
+    let videoName = randomVid.name;
+    vid.src = videoName + ".mp4";
 
-// const ChangeQrUrl = async (newLink = "https://google.com") =>
-// {
-//     const response = await fetch (`https://free.qrd.by/api/short?secretkey=98bed0c8c25f0f83f573c72decac3869&shorturl=wnu7m1&url=${newLink}`);
-//     const myJson = await response.json();
+    vid.onended = function() {
+        var sections = document.getElementsByClassName("section");
+        for(var i = 0; i < sections.length; i++){
+            sections[i].style.visibility = "visible";
+            
+            let optionText = data.videos[videoId].options[i].text;
+            let option = document.getElementsByClassName("section")[i];
+            option.innerHTML = optionText;
+            
+            if(option)
+        }
 
-//     console.log(myJson);
-// }
+        
+    
+    };
+});
 
 
 
 
-// function Video(name, option1, option2, option3, qrCode)
-// {
-//     this.name = name;
-//     this.option1 = option1;
-//     this.option2 = option2;
-//     this.option3 = option3;
-//     this.qrCode = qrCode;
-// }
-
-// let video1 = new Video("Work at Brainport", "eindhoven", "23");
