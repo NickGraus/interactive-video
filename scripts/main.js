063e58bc185a0aaf5f316d812279539c80ae1dba
NickGraus <nick-graus@hotmail.com>
2020-06-09 11:25:37 +0200

    options functionality working

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 let vid = document.getElementById("v0");
+var sections = document.getElementsByClassName("section");
 
 const fetchJSONFile = (path, callback) => {
     var httpRequest = new XMLHttpRequest();
@@ -21,22 +22,38 @@ fetchJSONFile("../JSON/videos.json", function(data) {
     vid.src = videoName + ".mp4";
 
     vid.onended = function() {
-        var sections = document.getElementsByClassName("section");
+
         for(var i = 0; i < sections.length; i++){
             sections[i].style.visibility = "visible";
             
             let optionText = data.videos[videoId].options[i].text;
             let option = document.getElementsByClassName("section")[i];
             option.innerHTML = optionText;
-            
-            if(option)
-        }
+            option.setAttribute("dest", data.videos[videoId].options[i].reference);
 
-        
-    
-    };
+            sections[i].addEventListener("click", (event) => {
+                videoName = event.target.getAttribute("dest");
+                vid.src = videoName + ".mp4";
+            })
+        }
+    }
+    vid.onplay = function() {
+        for(var i = 0; i < sections.length; i++){
+            sections[i].style.visibility = "hidden";
+        }
+    }
 });
 
 
 
 
+
+// sections[i].addEventListener("click"), (event) => {
+//     let input = event.target;
+//     console.log(input);
+//     for(var j = 0; j < data.videos[videoId].options.length; j++ ) {
+//         videoName = data.videos[videoId].options[j].reference;
+//         console.log(videoName);
+//     }
+    
+// }
