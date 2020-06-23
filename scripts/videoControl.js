84e69296e1f1d549e849aabc51e9ec25f3db668a
NickGraus <nick-graus@hotmail.com>
2020-06-23 20:56:05 +0200

    placed videos in folder

--- scripts/videoControl.js
+++ scripts/videoControl.js
@@ -3,6 +3,7 @@ const videoControl = (function () {
   let vid;
   let sections = [];
   let videoId;
+  let videoSource = "videos/";
 
   /**
    *  Setup all functions and makes them executable
@@ -28,7 +29,7 @@ const videoControl = (function () {
 
     videoId = randomVid.id;
     let videoName = randomVid.filename;
-    vid.src = videoName;
+    vid.src = videoSource + videoName;
   };
 
   /**
@@ -45,7 +46,7 @@ const videoControl = (function () {
 
         option.dataset.dest = data.videos[videoId].options[i].linkFilename;
         sections[i].addEventListener("click", (event) => {
-          vid.src = option.dataset.dest;
+          vid.src = videoSource + option.dataset.dest;
         });
       }
     };
@@ -67,21 +68,3 @@ const videoControl = (function () {
     Setup: Setup,
   };
 })();
-
-// Even samengevat mijn aanwijzingen en tips voor je video platform:
-
-// Ik zie in je code dat als je eenmaal een optie hebt aangeklikt er wel een nieuwe video gaat spelen, maar deze video zal dan geen overlays meer hebben. Eigenlijk moet je module zichzelf weer aanroepen op de nieuwe video. Je krijgt dan zoiets als:
-// sections[i].addEventListener("click", (event) => {
-//                 videoName = event.target.getAttribute("dest");
-//                 vid.src = videoName + ".mp4";
-
-// renderOverlay(optionsForThisVideo)
-//             })
-
-// De moeilijkheid is dan de nieuwe opties uit de JSON halen, je kan het filteren op naam:
-// data.find(i => {  return i.name === “Brainport at Work”  })
-
-// Uitbreidingen:
-// Linken aan het eerdere werk van Daan om 4 schermen aan te sturen.
-// QR code generator gebruiken; of lig plaatjes tonen bij de QR code
-// Extra opties toevoegen zoals: wanneer opties (aan het eind, op timecode etc.)
